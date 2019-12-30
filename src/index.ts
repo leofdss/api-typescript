d50884ff309080336011f489d2eb5d2f2c638c5d
Leonardo Farias de Souza Silva <leonardo.f.desouzasilva@gmail.com>
2019-12-30 15:14:13 -0300

    Config (#2)
    * Config (#2)

--- src/index.ts
+++ src/index.ts
@@ -1,91 +1,68 @@
-#!/usr/bin/env node
-
-/**
- * Module dependencies.
- */
-
-import app from '@app';
+import app from './app/app';
 import debug from 'debug';
 import http from 'http';
 import { AddressInfo } from 'net';
 
-/**
- * Get port from environment and store in Express.
- */
+class Server {
+  public server: http.Server;
+  public port: boolean | string | number;
 
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val: string) {
-  const n = parseInt(val, 10);
-
-  if (isNaN(n)) {
-    // named pipe
-    return val;
+  constructor() {
+    this.port = this.normalizePort(process.env.PORT || '3000');
+    app.set('port', this.port);
+    this.server = http.createServer(app);
+    this.server.listen(this.port);
+    this.server.on('error', this.onError);
+    this.server.on('listening', () => {
+      const addr = this.server.address() as AddressInfo;
+      const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+      debug('Listening on ' + bind);
+    });
   }
 
-  if (n >= 0) {
-    // port number
-    return n;
+  /* Normalize a port into a number, string, or false. */
+  private normalizePort(val: string): boolean | string | number {
+    const n = parseInt(val, 10);
+
+    if (isNaN(n)) {
+      // named pipe
+      return val;
+    }
+
+    if (n >= 0) {
+      // port number
+      return n;
+    }
+
+    return false;
   }
 
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error: any) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  const bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
+  /* Event listener for HTTP server "error" event. */
+  private onError(error: any): void {
+    if (error.syscall !== 'listen') {
       throw error;
+    }
+
+    const bind = typeof this.port === 'string'
+      ? 'Pipe ' + this.port
+      : 'Port ' + this.port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
   }
 }
 
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  const addr = server.address() as AddressInfo;
-  const bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
+const server = new Server();
